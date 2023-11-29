e88e47826327bbf471a9f77aa0dc8b1f32de0ad4
wwishh <wson1948@gmail.com>
2023-11-29 17:51:28 +0900

    day1129_검색결과 대표이미지 나오도록 수정

--- src/main/java/boot/data/mapper/InterestMapperInter.java
+++ src/main/java/boot/data/mapper/InterestMapperInter.java
@@ -1,9 +1,15 @@
 package boot.data.mapper;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface InterestMapperInter {
 	public void insertInterest(String i_id, int i_sangpum);
 	public void deleteInterest(String i_id, int i_sangpum);
+	//public int getCount(String i_id, int i_sangpum);
+    //public Map<String, Object> getAllLikeStatus(String i_id, ArrayList<Integer> i_sangList);
+
 }
